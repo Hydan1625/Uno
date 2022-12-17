170a7cd024523f0044d55e7c11082edff590a795
Hydan <90104826+Hydan1625@users.noreply.github.com>
2022-12-17 19:21:43 -0300

    Bora ver!

---
+++ main.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function preto() {
+    return (
+        <div className="hello world">
+            <p>Hey man</p>
+        </div>
+    )
+}
+
+
+ReactDOM.render(preto(p), document.getElementById('root'))
