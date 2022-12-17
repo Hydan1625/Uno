bf9ee4c1c181c1c0b67e7bb141ce838dc0de445c
Hydan <90104826+Hydan1625@users.noreply.github.com>
2022-12-17 20:00:09 -0300

    test

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 
 
-export default function preto() {
-    return (
-        <div className="hello world">
-            <p>Hey man</p>
-        </div>
-    )
-}
+// export default function preto() {
+//     return (
+//         <div className="hello world">
+//             <p>Hey man</p>
+//         </div>
+//     )
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
